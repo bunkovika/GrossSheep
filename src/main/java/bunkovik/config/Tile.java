eaf9fd5eaf2b3ebc552f7e6c6c0ad90cbafb2195
bunkovika <viktoriabunko28@gmail.com>
2023-04-28 15:00:10 +0200

    accepting the final idea of how making the tile(creating Tile class) adding GraphicContext.

--- src/main/java/bunkovik/config/Tile.java
+++ src/main/java/bunkovik/config/Tile.java
@@ -1,41 +1,24 @@
 package bunkovik.config;
 
-import javafx.scene.Node;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.paint.ImagePattern;
-import javafx.scene.shape.Rectangle;
 
-public class Tile  {
+public class Tile {
     private final Image image;
     private final boolean passable;
-    private final double x;
-    private final double y;
-
-    public Tile(String imagePath, boolean passable, double x, double y) {
+    public Tile(String imagePath, boolean passable) {
         this.passable = passable;
         this.image = new Image(imagePath);
-        this.x = x;
-        this.y = y;
     }
-
     public Image getImage() {
         return image;
     }
-
-    public void render(GraphicsContext gc) {
+    public void render(GraphicsContext gc, double x, double y) {
         gc.drawImage(image, x, y);
     }
-
     public boolean isPassable() {
         return passable;
     }
-
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return y;
-    }
 }
+
+
